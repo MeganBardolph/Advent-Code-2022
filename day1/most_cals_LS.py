f36799f17cee36f64addb4bf64b78b0be54c990c
leshelley <lauraeshelley@gmail.com>
2023-04-13 15:44:19 -0700

    adding some folders and my unfinished .py file

---
+++ day1/most_cals_LS.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 15:28:55 2023
+
+@author: laurashelley
+"""
+
+## first challenge is to find the elf carrying the most calories, and how many
+## calories is that?
+
+
+## change this to load a txt file - just has example data for accuracy check
+cals = [1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+]
+
+
+
+
+
