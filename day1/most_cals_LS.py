13fc0920fadf29906efb53836757a18b3bedfd61
leshelley <lauraeshelley@gmail.com>
2023-04-13 15:57:07 -0700

    changed example data from rtf to txt

--- day1/most_cals_LS.py
+++ day1/most_cals_LS.py
@@ -10,22 +10,8 @@ Created on Thu Apr 13 15:28:55 2023
 ## calories is that?
 
 
-## change this to load a txt file - just has example data for accuracy check
-cals = [1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000
-]
+## load input data from a txt file
+cals = open('inputdata_cals_exampledata.rtf').read() #example data - for accuracy check
 
 
 
